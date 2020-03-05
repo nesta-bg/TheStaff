661209a418c3613bc8f54a39d0cf47ea351020e7
nenadnesta <nenadnesta@gmail.com>
2020-03-05 09:43:13 +0100

    Datepickers in Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
@@ -22,6 +24,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
