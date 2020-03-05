ad2569cf1ca69da18d9f9a4a7be66cde87209da7
nenadnesta <nenadnesta@gmail.com>
2020-03-05 08:48:24 +0100

    Select List - Select Options from Array.

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,6 +8,13 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' }
+  ];
 
   constructor() { }
 
