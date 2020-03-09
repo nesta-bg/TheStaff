985d63efa17c766a8aa47f6745eab8f359fe156f
nenadnesta <nenadnesta@gmail.com>
2020-03-09 08:55:27 +0100

    Angular Select List Validation.

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -23,7 +23,7 @@ export class CreateEmployeeComponent implements OnInit {
     phoneNumber: null,
     email: null,
     dateOfBirth: null,
-    department: null,
+    department: '-1',
     isActive: null,
     photoPath: null
   };
