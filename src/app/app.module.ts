49f2c2ea0363ecc7afc408f4d58d15f1bdbbc901
nenadnesta <nenadnesta@gmail.com>
2020-03-09 17:36:57 +0100

    Passing Data from Parent to Child Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EmployeeService } from './employees/employee.service';
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { DisplayEmployeeComponent } from './employees/display-employee.component';
 
 const appRoutes: Routes = [
   { path: 'list', component: ListEmployeesComponent },
@@ -24,7 +25,8 @@ const appRoutes: Routes = [
     ListEmployeesComponent,
     CreateEmployeeComponent,
     SelectRequiredValidatorDirective,
-    ConfirmEqualValidatorDirective
+    ConfirmEqualValidatorDirective,
+    DisplayEmployeeComponent
   ],
   imports: [
     BrowserModule,
