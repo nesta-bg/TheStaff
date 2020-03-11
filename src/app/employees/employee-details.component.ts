4f1fba9bc67593d391d6bc946172bfb7688cc268
nenadnesta <nenadnesta@gmail.com>
2020-03-11 17:26:42 +0100

    Angular Read Route Parameters - Snapshot.

--- src/app/employees/employee-details.component.ts
+++ src/app/employees/employee-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from './employee.service';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-employee-details',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent implements OnInit {
+  employee: Employee;
 
-  constructor() { }
+  // tslint:disable-next-line: variable-name
+  constructor(private _route: ActivatedRoute, private _employeeService: EmployeeService) { }
 
   ngOnInit() {
+    const id = +this._route.snapshot.paramMap.get('id');
+    this.employee = this._employeeService.getEmployee(id);
   }
 
 }
