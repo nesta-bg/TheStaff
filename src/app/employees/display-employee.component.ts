904af061afac300d2bfb1cab3ec77a37ede1f57c
nenadnesta <nenadnesta@gmail.com>
2020-03-11 10:03:10 +0100

    Component Input Property Change Detection Using Property Setter - Example.

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,8 +6,19 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit, OnChanges {
-  @Input() employeeId: number;
+export class DisplayEmployeeComponent implements OnInit {
+  // tslint:disable-next-line: variable-name
+  private _employeeId: number;
+
+  @Input()
+  set employeeId(val: number) {
+    console.log('employeeId changed from ' + JSON.stringify(this._employeeId)
+      + ' to ' + JSON.stringify(val));
+    this._employeeId = val;
+  }
+  get employeeId(): number {
+    return this._employeeId;
+  }
 
   // tslint:disable-next-line: variable-name
   private _employee: Employee;
@@ -15,21 +26,14 @@ export class DisplayEmployeeComponent implements OnInit, OnChanges {
   // This property setter is called anytime the input property changes
   @Input()
   set employee(val: Employee) {
+    console.log('employee changed from ' + JSON.stringify(this._employee)
+    + ' to ' + JSON.stringify(val));
     this._employee = val;
   }
 
   // This getter is called when reading and displaying data
   get employee(): Employee {
     return this._employee;
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    for (const propName of Object.keys(changes)) {
-      const change = changes[propName];
-      const from = JSON.stringify(change.previousValue);
-      const to = JSON.stringify(change.currentValue);
-      console.log(propName + ' changed from ' + from + ' to ' + to);
-    }
   }
 
   constructor() { }
