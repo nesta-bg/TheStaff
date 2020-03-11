a2a44e40a0b589aa4dd13d119301560e0d7cf09f
nenadnesta <nenadnesta@gmail.com>
2020-03-11 09:48:06 +0100

    Component Input Property Change Detection Using ngOnChanges - Example.

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,21 +6,30 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
+export class DisplayEmployeeComponent implements OnInit, OnChanges {
+  @Input() employeeId: number;
+
   // tslint:disable-next-line: variable-name
   private _employee: Employee;
 
   // This property setter is called anytime the input property changes
   @Input()
   set employee(val: Employee) {
-    console.log('Previous : ' + (this._employee ? this._employee.name : 'NULL'));
-    console.log('Current : ' + val.name);
     this._employee = val;
   }
 
   // This getter is called when reading and displaying data
   get employee(): Employee {
     return this._employee;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName of Object.keys(changes)) {
+      const change = changes[propName];
+      const from = JSON.stringify(change.previousValue);
+      const to = JSON.stringify(change.currentValue);
+      console.log(propName + ' changed from ' + from + ' to ' + to);
+    }
   }
 
   constructor() { }
