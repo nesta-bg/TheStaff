c48b092c9640709e602a773348d7c89972fb8c6b
nenadnesta <nenadnesta@gmail.com>
2020-03-17 11:57:28 +0100

    Angular HttpClient Put.

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -59,20 +59,33 @@ export class CreateEmployeeComponent implements OnInit {
       this.createEmployeeForm.reset();
       this.panelTitle = 'Create Employee';
     } else {
-      this.employee = Object.assign({}, this._employeeService.getEmployee(id));
+      this._employeeService.getEmployee(id).subscribe(
+        (employee) => { this.employee = employee; },
+        (err: any) => console.log(err)
+      );
       this.panelTitle = 'Edit Employee';
     }
   }
 
   saveEmployee(): void {
-    this._employeeService.save(this.employee).subscribe(
-      (data: Employee) => {
-        console.log(data);
-        this.createEmployeeForm.reset();
-        this._router.navigate(['list']);
-      },
-      (error: any) => { console.log(error); }
-    );
+    if (this.employee.id == null) {
+      this._employeeService.addEmployee(this.employee).subscribe(
+        (data: Employee) => {
+          console.log(data);
+          this.createEmployeeForm.reset();
+          this._router.navigate(['list']);
+        },
+        (error: any) => { console.log(error); }
+      );
+    } else {
+      this._employeeService.updateEmployee(this.employee).subscribe(
+        () => {
+          this.createEmployeeForm.reset();
+          this._router.navigate(['list']);
+        },
+        (error: any) => { console.log(error); }
+      );
+    }
   }
 
   togglePhotoPreview() {
--- src/app/employees/employee-details-guard-service.ts
+++ src/app/employees/employee-details-guard-service.ts
@@ -1,6 +1,8 @@
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { EmployeeService } from './employee.service';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable()
 // CanActivate - Go to Definition
@@ -8,15 +10,23 @@ export class EmployeeDetailsGuardService implements CanActivate {
     // tslint:disable-next-line: variable-name
     constructor(private _employeeService: EmployeeService, private _router: Router) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        // to convert the result to boolean we use !!
-        const employeeExists = !!this._employeeService.getEmployee(+route.paramMap.get('id'));
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        return this._employeeService.getEmployee(+route.paramMap.get('id'))
+        .pipe(
+            map(employee => {
+                const employeeExists = !!employee;
 
-        if (employeeExists) {
-            return true;
-        } else {
-            this._router.navigate(['/notfound']);
-            return false;
-        }
+                if (employeeExists) {
+                    return true;
+                } else {
+                    this._router.navigate(['notfound']);
+                    return false;
+                }
+            }),
+            catchError((err) => {
+                console.log(err);
+                return of (false);
+            })
+        );
     }
 }
